d79b43ad2318997db064cb934df6fab7d416b6a3
华为Matebook14 <zcw1216547609@gmail.com>
2025-05-14 10:56:05 +0800

    暂时设置固定密钥方便测试

--- src/main/java/com/scut/industrial_software/utils/JwtUtils.java
+++ src/main/java/com/scut/industrial_software/utils/JwtUtils.java
@@ -6,13 +6,22 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 
 import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import java.util.Date;
 import java.util.Map;
 
 public class JwtUtils {
-    // 生成安全的密钥（推荐）
+    /*// 生成安全的密钥（推荐）
     private static final SecretKey SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private static final long EXPIRATION_TIME = 12 * 60 * 60 * 1000; // 12小时*/
+
+
+    // 使用固定的密钥字符串
+    private static final String SECRET_KEY_STRING = "your-fixed-secret-key-asdfghjkl0202020202";  // 固定的密钥
     private static final long EXPIRATION_TIME = 12 * 60 * 60 * 1000; // 12小时
+
+    // 将固定的密钥字符串转换为 SecretKey
+    private static final SecretKey SECRET_KEY = new SecretKeySpec(SECRET_KEY_STRING.getBytes(), SignatureAlgorithm.HS256.getJcaName());
 
     /**
      * 生成 JWT 令牌
