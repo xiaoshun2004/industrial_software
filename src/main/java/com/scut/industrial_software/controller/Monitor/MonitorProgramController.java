a741af0ead6f1f7f71bbca75d8344156cd7b4314
Ming <913237300@qq.com>
2025-11-05 15:08:28 +0800

    任务监控功能修复II

--- src/main/java/com/scut/industrial_software/controller/Monitor/MonitorProgramController.java
+++ src/main/java/com/scut/industrial_software/controller/Monitor/MonitorProgramController.java
@@ -1,15 +1,13 @@
 package com.scut.industrial_software.controller.Monitor;
 
 import com.scut.industrial_software.common.api.ApiResult;
-import com.scut.industrial_software.model.vo.MonitorVO;
 import com.scut.industrial_software.service.IMonitorService;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 @RestController
@@ -22,21 +20,32 @@ public class MonitorProgramController {
     /**
      * 启动exe程序
      */
+    /*
     @PostMapping("/start")
     public ApiResult<String> start(){
         // 启动exe程序
-        CompletableFuture<String> future = monitorService.startProgram();
-        return ApiResult.success(future.join());
+        return monitorService.startProgram();
     }
+    */
 
     /**
-     * 使用Sigar监控本地exe程序
+     * 停止exe程序
      */
     /*
-    @PostMapping("/monitorProgram")
-    public ApiResult<MonitorVO> monitorProgram(Long pid) {
+    @PostMapping("/stop")
+    public ApiResult<String> stopProgram(Long pid) {
         // 获取当前时间调用的exe程序的监控资源
-        return monitorService.monitorProgram(pid);
+        return monitorService.stopProgram(pid);
     }*/
+
+    /**
+     * 获取程序状态
+     */
+    /*
+    @GetMapping("/status")
+    public ApiResult<String> getStatus(Long pid){
+        return monitorService.getProgramStatus(pid);
+    }*/
+
 
 }
